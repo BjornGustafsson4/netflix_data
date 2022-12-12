c884335b67ff0efcb407ba27a7cffc05dfd4f63a
BjornGustafsson4 <obxband102@gmail.com>
2022-12-12 11:23:40 -0500

    Organized the script

--- netflix_data.py
+++ netflix_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import collections
 
 
-netflix_csv = pd.read_csv("C:\\Users\\Björn\\PythonVenv\\netflix_data\\netflix-report\\CONTENT_INTERACTION\\ViewingActivity.csv", usecols = ['Profile Name', 'Start Time']) 
+netflix_csv = pd.read_csv("C:\\Users\\Björn\\PythonVenv\\netflix_data\\netflix-report\\CONTENT_INTERACTION\\ViewingActivity.csv", usecols = ['Start Time']) 
 #Gets hours from datetime directly from csv and converts from UTC to EST, as well pulls dates
 hours = []
 dates = []
@@ -18,29 +18,25 @@ for datetime in netflix_csv["Start Time"]:
     dates.append(f"{d.day_of_week} {d.day_name()}")
 
 
-#counts, organizes, and turns hours into percentages
+#produces a bar graph for hours
 hour_dict = collections.Counter(hours)
 hour_dict = collections.OrderedDict(sorted(hour_dict.items()))
 for key, value in hour_dict.items():
     hour_dict[key] = (value/len(netflix_csv["Start Time"]))*100
-
-
-
 hour_keys = hour_dict.keys()
 hour_values = hour_dict.values()
-plt.title("Probability of watching Netflix based on time")
+plt.title("Probability of watching Netflix in a given hour")
 plt.xlabel("Hours in a day (24 hour clock)")
 plt.ylabel("Probability of watching Netflix")
 plt.bar(hour_keys, hour_values)
 plt.show()
 
 
+#produces a bar graph for days
 dates_dict = collections.Counter(dates)
 dates_dict = collections.OrderedDict(sorted(dates_dict.items()))
 for key, value in dates_dict.items():
     dates_dict[key] = (value/len(netflix_csv["Start Time"]))*100
-
-
 date_keys = dates_dict.keys()
 date_values = dates_dict.values()
 plt.title(f"Percentage of all Netflix viewed by day of the Week")
